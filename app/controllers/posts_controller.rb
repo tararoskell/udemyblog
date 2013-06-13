419e3fdcd153c5d285f200700a66385c47715ad9
tararoskell <tara@troskell.demon.co.uk>
2013-06-13 14:26:32 +0100

    Add post and category models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = "This is sample text for our Ruby Blog"
+    @content_second = "This is some sample text for the blog. Just some gobbledygook"
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def show
+    
+  end
+  
+  def destroy
+    
+  end
+  
+end
